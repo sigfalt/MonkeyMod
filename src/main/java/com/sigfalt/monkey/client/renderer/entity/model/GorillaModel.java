997e3330eb1a39b11ec93a79aee61e9321ab0365
sigfalt <1689082+sigfalt@users.noreply.github.com>
2021-03-27 22:29:15 -0500

    Fix child gorilla rendering

--- src/main/java/com/sigfalt/monkey/client/renderer/entity/model/GorillaModel.java
+++ src/main/java/com/sigfalt/monkey/client/renderer/entity/model/GorillaModel.java
@@ -1,31 +1,39 @@
 package com.sigfalt.monkey.client.renderer.entity.model;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.renderer.entity.model.EntityModel;
+import com.google.common.collect.ImmutableList;
+import com.sigfalt.monkey.common.entity.GorillaEntity;
+import net.minecraft.client.renderer.entity.model.AgeableModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraft.entity.Entity;
 
-public class GorillaModel<T extends Entity> extends EntityModel<T> {
-	private final ModelRenderer bb_main;
+public class GorillaModel<T extends GorillaEntity> extends AgeableModel<T> {
+	private final ModelRenderer body;
+	private final ModelRenderer head;
 
 	public GorillaModel() {
+		super(false, 12.0F, 4.0F);
 		textureWidth = 128;
 		textureHeight = 128;
 
-		bb_main = new ModelRenderer(this);
-		bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
-		bb_main.setTextureOffset(0, 0).addBox(-16.0F, -32.0F, -16.0F, 32.0F, 32.0F, 32.0F, 0.0F, false);
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 24.0F, 0.0F);
+		body.setTextureOffset(0, 0).addBox(-12.0F, -24.0F, -8.0F, 24.0F, 24.0F, 24.0F, 0.0F, false);
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(0.0F, 24.0F, 0.0F);
+		head.setTextureOffset(0, 48).addBox(-4.0F, -32.0F, -14.0F, 8.0F, 10.0F, 8.0F, 0.0F, false);
 	}
 
 	@Override
-	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-		//previously the render function, render code was moved to a method below
+	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){}
+
+	@Override
+	protected Iterable<ModelRenderer> getHeadParts() {
+		return ImmutableList.of(this.head);
 	}
 
 	@Override
-	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-		bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
+	protected Iterable<ModelRenderer> getBodyParts() {
+		return ImmutableList.of(this.body);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
